9602ca18441f7c853dd941e31eff13740148ae9d
lizozom <liza.katz@elastic.co>
2022-07-28 15:25:38 +0300

    secure always og tags mobile theming

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
+const appName = 'Duckpuc';
+const duckpuckDescription = 'Duckpuc - Fun & Concensual dickpics';
+
 module.exports = {
     entry: path.resolve(__dirname, 'index.tsx'),
     output: {
@@ -67,18 +70,36 @@ module.exports = {
         ],
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
+        new HtmlWebpackPlugin({ 
+            template: path.resolve(__dirname, 'index.html'),
+            meta: {
+                viewport: 'width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no',
+                'description': duckpuckDescription,
+                'keyword': 'duckpuc, dickpic, dick, penis, photo, fun, concensual, conscent' ,
+                'og:title': appName,
+                'og:description': { property: 'og:description', content: duckpuckDescription},
+                'og:type': { property: 'og:type', content: 'website' },
+                'og:url': { property: 'og:url', content: 'https://duckpuc.com/' },
+                // 'og:image': { property: 'og:image', content: '...' },
+                'twitter:card': { name: 'twitter:card', content: 'summary_large_image' },
+                'twitter:title': { name: 'twitter:title', content: appName },
+                'twitter:description': { name: 'twitter:description', content: duckpuckDescription },
+                // 'twitter:image': { name: 'twitter:image', content: '...' }
+            }
+        }),
         new FaviconsWebpackPlugin({
             logo: './public/duckpuc-logo.svg', // svg works too!
             mode: 'webapp', // optional can be 'webapp', 'light' or 'auto' - 'auto' by default
             devMode: 'webapp', 
             favicons: {
                 appName: 'duckpuc',
-                appDescription: 'Duckpuc - Fun & Concensual dickpics',
+                appDescription: duckpuckDescription,
                 developerName: 'lizozom',
                 developerURL: null, // prevent retrieving from the nearest package.json
-                background: '#ddd',
-                theme_color: '#333',
+                background: '#feece0',
+                theme_color: '#feece0',
+                display: 'fullscreen',
+                orientation: 'portrait',
                 icons: {
                   coast: false,
                   yandex: false
