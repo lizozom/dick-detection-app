b46294e03930c5ed07c4ad53bf0e296b8587ebd2
lizozom <liza.katz@elastic.co>
2022-08-01 17:45:07 +0300

    webpack split

---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp",
+        },
+        historyApiFallback: true,
+    },
+});
