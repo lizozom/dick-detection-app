18ac470b9107cbba97ceb34a7b8c9150915e752a
lizozom <liza.katz@elastic.co>
2022-08-02 19:32:56 +0300

    use yolo events

--- src/hooks/use_yolo.ts
+++ src/hooks/use_yolo.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { simd, threads } from 'wasm-feature-detect';
+import ReactGA from 'react-ga4';
 
 interface WasmModule {
   onRuntimeInitialized?: () => void;
@@ -17,10 +18,21 @@ export function useYolo() {
       let has_threads: boolean = false;
       let yolo_module_name: string = '';
 
+      ReactGA.event({
+        category: 'ml',
+        action: 'use_yolo',
+        nonInteraction: true,
+      });
+
       var wasmModuleLoaded = false;
       const wasmModuleLoadedCallbacks: Array<()=> void> = [];
 
       Module.onRuntimeInitialized = function() {
+          ReactGA.event({
+            category: 'ml',
+            action: 'runtime_init',
+            nonInteraction: true,
+          });
           wasmModuleLoaded = true;
           for (var i = 0; i < wasmModuleLoadedCallbacks.length; i++) {
               wasmModuleLoadedCallbacks[i]();
@@ -35,7 +47,7 @@ export function useYolo() {
           if (has_simd && has_threads)
           {
               yolo_module_name = 'Sample';
-              console.log('simd&threads enabled.');
+              // console.log('simd&threads enabled.');
           }
           else
           {
@@ -43,7 +55,14 @@ export function useYolo() {
               console.error('cannot enable simd&threads.');
           }
 
-          console.log('load ' + yolo_module_name);
+          ReactGA.event({
+            category: 'ml',
+            action: 'wasm_module_choice',
+            label: yolo_module_name,
+            nonInteraction: true,
+          });
+
+          // console.log('load ' + yolo_module_name);
 
           var yolowasm = yolo_module_name + '.wasm';
           var yolojs = yolo_module_name + '.js';
@@ -51,14 +70,41 @@ export function useYolo() {
           fetch(yolowasm)
           .then(response => response.arrayBuffer())
           .then(buffer => {
+              ReactGA.event({
+                category: 'ml',
+                action: 'wasm_buffer_loaded',
+                label: yolo_module_name,
+                nonInteraction: true,
+              });
               Module.wasmBinary = buffer;
               var script = document.createElement('script');
               script.src = yolojs;
               script.onload = function() {
+                  ReactGA.event({
+                    category: 'ml',
+                    action: 'wasm_loaded',
+                    label: yolo_module_name,
+                    nonInteraction: true,
+                  });
                   console.log('Emscripten boilerplate loaded.');
                   setLoaded(true);
               }
+              script.onerror = function() {
+                ReactGA.event({
+                  category: 'ml',
+                  action: 'wasm_script_load_error',
+                  label: yolo_module_name,
+                  nonInteraction: true,
+                });
+              }
               document.body.appendChild(script);
+          }).catch(() => {
+            ReactGA.event({
+              category: 'ml',
+              action: 'wasm_buffer_load_error',
+              label: yolo_module_name,
+              nonInteraction: true,
+            });
           });
 
         })
