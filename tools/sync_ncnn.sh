d9298ba2e22134981fc3c96fa1a76d1355ee136c
lizozom <liza.katz@elastic.co>
2022-08-14 13:42:33 +0300

    sync script

---
+++ tools/sync_ncnn.sh
@@ -0,0 +1,4 @@
+cp -a ~/Projects/ncnn/build/install/.                     ./cpp/ncnn-ios-old/
+cp -a ~/Projects/ncnn/build-simd/install/.                ./cpp/ncnn-ios-new/
+cp -a ~/Projects/ncnn/build-simd-threads/install/.        ./cpp/ncnn/
+
